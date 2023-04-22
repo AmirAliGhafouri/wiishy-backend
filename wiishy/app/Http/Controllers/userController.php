12103a01e40a7309a743b621f8866954ea85f430
amirali <amiralighafouri80@gmail.com>
2023-04-22 10:19:43 +0430

    use status to find userProfile

--- wiishy/app/Http/Controllers/userController.php
+++ wiishy/app/Http/Controllers/userController.php
@@ -10,27 +10,8 @@ class userController extends Controller
 {
 //_____________________ User Profile
     function user_profile($user_id){
-        try{
-            $user=User::findOrFail($user_id);
-        }
-        catch(\Exception $exception){
-            return response(['message'=>'user not found'] , 500);
-        }
+        $user=User::where(['id'=>$user_id,'status'=>1])->first();
         return response(['user'=>$user]);
     }
 
-     /*  function adduser(Request $req){
-        $user= new user;
-        $user->name=$req->name;
-        $user->family=$req->family;
-        $user->userBirthday=$req->birth;
-        $user->userLocationid=$req->loc;
-        $user->userGender=$req->gender;
-        $user->userDescription=$req->desc;
-        $user->userImageUrl=$req->url;
-        $user->status=$req->status;
-        $user->userCode=$req->code;
-        $user->save();
-        return response(['message'=>'done'],200);
-    } */
 }
