1519e7528edf077e21560fc619fa1476d3ead187
amirali <amiralighafouri80@gmail.com>
2023-05-02 17:27:29 +0430

    user-gift & gift-detail & followings-goft refactored

--- wiishy/app/Http/Controllers/giftsController.php
+++ wiishy/app/Http/Controllers/giftsController.php
@@ -7,6 +7,7 @@ use App\Models\gift;
 use App\Models\giftlike;
 use App\Models\giftUser;
 use App\Repositories\giftRepository;
+use App\Repositories\likeRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -15,36 +16,20 @@ class giftsController extends Controller
     
 //_____________________ All the gifts of a user
     function user_gifts($user_id){
-        $gifts=DB::table('giftuser')
-        ->join('gifts','giftuser.gift_id','=','gifts.id')
-        ->where(['giftuser.user_id'=>$user_id , 'gift_status'=>1])
-        ->select('giftuser.gift_id' , 'giftuser.gift_view' , 'giftuser.gift_like' , 'giftuser.desire_rate' , 'giftuser.created_at' , 'giftName' , 'giftPrice' , 'giftDesc' , 'giftUrl')
-        ->get();
+        $gifts=giftRepository::user_gift($user_id);
         return response(['gifts'=>$gifts]);
     }
 
 //_____________________ A complete gift detail of a user
     function gift_detail($gift_id , $user_id){
-        $gift_detail=DB::table('giftuser')
-        ->join('users','users.id','=','giftuser.user_id')
-        ->join('gifts','gifts.id','=','giftuser.gift_id')
-        ->where(['giftuser.user_id'=>$user_id , 'giftuser.gift_id'=>$gift_id , 'users.status'=>1 , 'gift_status'=>1])
-        ->select('user_id','gift_id','giftName','giftPrice','giftDesc','giftUrl','giftImageUrl','gift_like','gift_view','shared','desire_rate','giftuser.created_at','name','family','userImageUrl')
-        ->get();
-        $like=$this->like_check($gift_id , $user_id);
+        $gift_detail=giftRepository::gift_details($gift_id , $user_id);
+        $like=likeRepository::check($gift_id , $user_id);
         return response(['like'=>$like,'gift_detail'=>$gift_detail]);
     }
 
 //_____________________ All the gifts of the users followings
     function followings_gift($id){
-        $gifts=DB::table('giftuser')
-        ->join('users','giftuser.user_id','=','users.id')
-        ->join('userfollows','giftuser.user_id','=','userfollows.follow_id')
-        ->join('gifts','gifts.id','=','giftuser.gift_id')
-        ->where('userfollows.user_id',$id )
-        ->where(['follow_status'=>1 , 'gift_status'=>1])
-        ->select('giftuser.user_id','giftuser.gift_id','giftName','giftUrl','giftImageUrl','gift_like','giftuser.created_at as giftuser_created_at','name','family','userImageUrl')
-        ->get()->sortByDesc('giftuser_created_at')->values();
+        $gifts=giftRepository::followings_gift($id);
         $count=$gifts->count();
         return response(['followings_gifts_count'=>$count ,'followings_gifts'=>$gifts]);
     }
--- wiishy/app/Repositories/giftRepository.php
+++ wiishy/app/Repositories/giftRepository.php
@@ -2,11 +2,38 @@
 namespace App\Repositories;
 
 use App\Models\gift;
-use App\Models\giftlike;
 use App\Models\giftUser;
+use Illuminate\Support\Facades\DB;
 
 class giftRepository
 {
+    static function user_gift($user_id){
+        return DB::table('giftuser')
+        ->join('gifts','giftuser.gift_id','=','gifts.id')
+        ->where(['giftuser.user_id'=>$user_id , 'gift_status'=>1])
+        ->select('giftuser.gift_id' , 'giftuser.gift_view' , 'giftuser.gift_like' , 'giftuser.desire_rate' , 'giftuser.created_at' , 'giftName' , 'giftPrice' , 'giftDesc' , 'giftUrl')
+        ->get();
+    }
+
+    static function gift_details($gift_id , $user_id){
+        return DB::table('giftuser')
+        ->join('users','users.id','=','giftuser.user_id')
+        ->join('gifts','gifts.id','=','giftuser.gift_id')
+        ->where(['giftuser.user_id'=>$user_id , 'giftuser.gift_id'=>$gift_id , 'users.status'=>1 , 'gift_status'=>1])
+        ->select('user_id','gift_id','giftName','giftPrice','giftDesc','giftUrl','giftImageUrl','gift_like','gift_view','shared','desire_rate','giftuser.created_at','name','family','userImageUrl')
+        ->get();
+    }
+
+    static function followings_gift($id){
+        return DB::table('giftuser')
+        ->join('users','giftuser.user_id','=','users.id')
+        ->join('userfollows','giftuser.user_id','=','userfollows.follow_id')
+        ->join('gifts','gifts.id','=','giftuser.gift_id')
+        ->where('userfollows.user_id',$id )
+        ->where(['follow_status'=>1 , 'gift_status'=>1])
+        ->select('giftuser.user_id','giftuser.gift_id','giftName','giftUrl','giftImageUrl','gift_like','giftuser.created_at as giftuser_created_at','name','family','userImageUrl')
+        ->get()->sortByDesc('giftuser_created_at')->values();
+    }
 
     static function increase($gift_id , $field){
         giftUser::where('gift_id',$gift_id)->increment($field);
