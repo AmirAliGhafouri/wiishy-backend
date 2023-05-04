f393a28c33af0bf5d86051363277f04c64488722
amirali <amiralighafouri80@gmail.com>
2023-05-04 17:29:13 +0430

    migration giftlike

---
+++ wiishy/database/migrations/2023_05_04_125715_create_giftlike.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('giftlike', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->bigInteger('gift_id');
+            $table->integer('like_status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('giftlike');
+    }
+};
