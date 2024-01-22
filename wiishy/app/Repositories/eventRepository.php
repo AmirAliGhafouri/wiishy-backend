baf8c708ce5a37bab539892cfdb70413ed22137a
amirali <amiralighafouri80@gmail.com>
2024-01-22 12:10:24 +0330

    correcting syntax of eventRepository

--- wiishy/app/Repositories/eventRepository.php
+++ wiishy/app/Repositories/eventRepository.php
@@ -8,41 +8,55 @@ use Carbon\Carbon;
 
 class eventRepository
 {
-    static function create($req){
+    /**
+     * create
+     * 
+     * @param array $req
+     * @return array
+     */
+    static function create($req)
+    {
         return userevent::create($req); 
     }
 
-    static function update($event_id,$request){
-        return userevent::where('id',$event_id)->update($request); 
+    static function update($event_id, $request)
+    {
+        return userevent::where('id', $event_id)->update($request); 
     }
 
-    static function get($event_id,$user_id){
-        return userevent::where(['user_id'=>$user_id ,'id'=>$event_id , 'status'=>1])->first(); 
+    static function get($event_id, $user_id)
+    {
+        return userevent::where(['user_id' => $user_id, 'id' => $event_id, 'status' => 1])->first(); 
     }
 
-    static function destroy($event_id){
-        return userevent::where(['id'=>$event_id , 'status'=>1])->update(['status'=>0]); 
+    static function destroy($event_id)
+    {
+        return userevent::where(['id' => $event_id, 'status' => 1])->update(['status' => 0]); 
     }
 
-    static function detail($user_id,$event_id){
-        return userevent::where(['user_id'=>$user_id ,'id'=>$event_id ,'status'=>1])->first(); 
+    static function detail($user_id, $event_id)
+    {
+        return userevent::where(['user_id' => $user_id, 'id' => $event_id, 'status' => 1])->first(); 
     }
 
-    static function list($user_id){
-        return userevent::where(['user_id'=>$user_id,'status'=>1])->get()->sortByDesc('created_at')->values();
+    static function list($user_id)
+    {
+        return userevent::where(['user_id' => $user_id, 'status' => 1])->get()->sortByDesc('created_at')->values();
     }
 
-    static function type($id){
+    static function type($id)
+    {
         try{
-            return event::where('id',$id)->first()->name;
+            return event::where('id', $id)->first()->name;
         }
         catch(\Exception $exception){
             return "none";
         }
     }
 
-    static function remaining_days($date){
-        $event_date= Carbon::create($date);
+    static function remaining_days($date)
+    {
+        $event_date = Carbon::create($date);
         $currentDate = Carbon::now();
         $eventThisYear = $event_date->copy()->year($currentDate->year);
         if ($currentDate > $eventThisYear) {
@@ -51,20 +65,23 @@ class eventRepository
         return  now()->diffInDays(Carbon::parse($eventThisYear));
     }
 
-    static function rel_type($relation){
+    static function rel_type($relation)
+    {
         try{
-            return relationship::where('id',$relation)->first()->name;
+            return relationship::where('id', $relation)->first()->name;
         }
         catch(\Exception $exception){
             return "none";
         }
     }
 
-    static function events(){
+    static function events()
+    {
         return event::all();
     }
 
-    static function relationships(){
+    static function relationships()
+    {
         return relationship::all();
     }
 }
