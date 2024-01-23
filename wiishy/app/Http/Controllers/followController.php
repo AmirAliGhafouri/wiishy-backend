f7bb1f7fcdbc4e02f36b0f7114ea8d19f9330fad
amirali <amiralighafouri80@gmail.com>
2024-01-23 14:50:36 +0330

    optimize followings

--- wiishy/app/Http/Controllers/followController.php
+++ wiishy/app/Http/Controllers/followController.php
@@ -32,6 +32,7 @@ class followController extends Controller
         $user_id = $req->user()->id;
         $followers = followRepository::follow_list($req->id, 'userfollows.user_id', 'userfollows.follow_id');
         $list = followerListResource::collection($followers, $user_id);
+        
         return response([
             'status' => 'success',
             'followers_count' => $followers_count,
@@ -39,25 +40,30 @@ class followController extends Controller
         ], 200);
     }  
     
-//_____________________ All the following of a user 
+    /**
+     * followings of a user
+     * 
+     * @param \Illuminate\Http\Request $req
+     */
     function user_followings(Request $req){
         try{
-            $followings_count=followRepository::count($req->id,'followings');
-        }
-        catch(\Exception $exception){
+            $followings_count = followRepository::count($req->id, 'followings');
+        } catch(\Exception $exception){
             return response([
-                'status'=>'Error',
-                'message'=>'user not found'
+                'status' => 'Error',
+                'message' => 'user not found'
             ] , 400);
         }
-        $user_id=$req->user()->id;
-        $followings=followRepository::follow_list($req->id,'userfollows.follow_id','userfollows.user_id');  
-        $list= followingListResource::collection($followings,$user_id);
+
+        $user_id = $req->user()->id;
+        $followings = followRepository::follow_list($req->id, 'userfollows.follow_id', 'userfollows.user_id');  
+        $list = followingListResource::collection($followings, $user_id);
+
         return response([
-            'status'=>'success',
-            'followings_count'=>$followings_count,
-            'followings'=>$list
-        ],200);
+            'status' => 'success',
+            'followings_count' => $followings_count,
+            'followings' => $list
+        ], 200);
     }
 
 //_____________________ Suggestions to users to follow
