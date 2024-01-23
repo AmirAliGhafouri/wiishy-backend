3e516b7fae83b1bec6b291909cc77d0fed570d3a
amirali <amiralighafouri80@gmail.com>
2024-01-23 08:48:25 +0330

    add comment to userProfile and home

--- wiishy/app/Http/Controllers/userController.php
+++ wiishy/app/Http/Controllers/userController.php
@@ -16,49 +16,68 @@ use App\Repositories\giftRepository;
 
 class userController extends Controller
 {
-//_____________________ User Profile
-    function user_profile($user_id){
-        $user=userRepository::all($user_id);
-        if(!$user){
+    /**
+     * All detail of a user
+     * 
+     * @param int $user_id
+     * @return \Illuminate\Http\Response|\Illuminate\Contracts\Routing\ResponseFactory
+     */
+    public function user_profile($user_id)
+    {
+        $user = userRepository::all($user_id);
+
+        // checing if user is exist
+        if (!$user) {
             return response([
-                'status'=>'Error',
-                'message'=>'not found'
-            ],400);
+                'status' => 'Error',
+                'message' => 'not found'
+            ], 400);
         }
-        $profile=userProfileResource::make($user);
-        return response(['users'=>$profile]);
+
+        $profile = userProfileResource::make($user);
+        return response([
+            'status' => 'sucess',
+            'users' => $profile
+            ]);
     }
 
-//_____________________ Home Page
+    /**
+     * Home page where you can see followings gifts
+     * 
+     * @param \Illuminate\Http\Request $req
+     * @return \Illuminate\Http\Response|\Illuminate\Contracts\Routing\ResponseFactory
+     */
     function home(Request $req){
-        $user_id=$req->user()->id;
-        $user=userRepository::all($user_id);
-        $gifts=giftRepository::all($user_id);
+        $user_id = $req->user()->id;
+        $user = userRepository::all($user_id);
+        $gifts = giftRepository::all($user_id);
             return response([
-                'status'=>'success',
-                'user'=>$user,
-                'gifts'=>$gifts
-            ],200);
+                'status' => 'success',
+                'user' => $user,
+                'gifts' => $gifts
+            ], 200);
     }
 
 //_____________________ User List
     function user_list(){
-        $list=userRepository::list();
-        return response(['user'=>$list]);
+        $list = userRepository::list();
+        return response(['user' => $list]);
     }
 
 //_____________________ Remove User
     function remove($user_id){
-        $user=userRepository::destroy($user_id);
-        if(!$user)
+        $user = userRepository::destroy($user_id);
+        if (!$user) {
             return response([
-                'status'=>'Error',
-                'message'=>'User not found'
-            ],400);
+                'status' => 'Error',
+                'message' => 'User not found'
+            ], 400);
+        }
+          
         return response([
-            'status'=>'success',
-            'message'=>'User is removed successfully'
-        ],200);
+            'status' => 'success',
+            'message' => 'User is removed successfully'
+        ], 200);
     }
 
 //_____________________ Update User
@@ -89,33 +108,35 @@ class userController extends Controller
 
 //_____________________ Update User
     function update(UpdateUserRequest $req){
-        Log::debug('updateUserRequest',[$req->all()]);
+        Log::debug('updateUserRequest', [$req->all()]);
         if(!$req->all()){
             return response([
-                'status'=>'Error',
-                'message'=>'Empty request'
-            ],400);
+                'status' => 'Error',
+                'message' => 'Empty request'
+            ], 400);
         }
-        $user_id=$req->user()->id;
-        $user=userRepository::get($user_id);
-        if(!$user)
+        $user_id = $req->user()->id;
+        $user = userRepository::get($user_id);
+        if(!$user) {
             return response([
-                'status'=>'Error',
-                'message'=>'User not found'
-            ],400);
-        $request =collect($req->validated())->filter(function($item){
+                'status' => 'Error',
+                'message' => 'User not found'
+            ], 400);
+        }
+           
+        $request = collect($req->validated())->filter(function($item){
             return $item != null;
         })->toArray();
         if($req->image){
             $fileName = $req->user()->id.'.'.$req->image->getClientOriginalExtension();
-            Storage::disk('public')->putFileAs('/users',$req->image,$fileName);
+            Storage::disk('public')->putFileAs('/users', $req->image, $fileName);
             unset($request['image']);
             $request['user_image_url'] = '/uploads/users/' . $fileName.'?t='.Carbon::now()->getTimestamp();
         }
         userRepository::update($user_id, $request);       
         return response([
-            'status'=>'success',
-            'message'=>'UserProfile has updated successfully'
+            'status' => 'success',
+            'message' => 'UserProfile has updated successfully'
         ]);
     }
 
@@ -123,58 +144,61 @@ class userController extends Controller
 
 
 //_____________________ Auth
-    function auth(CreateUserRequest $req ,$provider){
-        $user_provider=userRepository::provider($provider);
-        if(!$user_provider){
+    function auth(CreateUserRequest $req, $provider){
+        $user_provider = userRepository::provider($provider);
+        if (!$user_provider) {
             return response([
-                'status'=>'Error',
-                'message'=>'Wrong provider'
-            ],400);
+                'status' => 'Error',
+                'message' => 'Wrong provider'
+            ], 400);
         }        
 
-        $user=userRepository::check($req,$user_provider->id);
-        if(!$user){
-            $req['provider_id']=$user_provider->id;
-            $register=userRepository::create($req->toArray());
-            $token= userRepository::token($register);
+        $user = userRepository::check($req,$user_provider->id);
+        if (!$user) {
+            $req['provider_id'] = $user_provider->id;
+            $register = userRepository::create($req->toArray());
+            $token = userRepository::token($register);
             return response([
-                'status'=>'success',
-                'new_user'=>true,
-                'message'=>'Registration is done successfully',
-                'token'=>$token,
-                'user'=>$register
-            ],200);
+                'status' => 'success',
+                'new_user' => true,
+                'message' => 'Registration is done successfully',
+                'token' => $token,
+                'user' => $register
+            ], 200);
         }
 
-        $token= userRepository::token($user);
+        $token = userRepository::token($user);
         return response([
-            'status'=>'success',
-            'new_user'=>false,
-            'message'=>'User logged in successfully',
-            'token'=>$token,
-            'user'=>$user
-        ],200);       
+            'status' => 'success',
+            'new_user' => false,
+            'message' => 'User logged in successfully',
+            'token' => $token,
+            'user' => $user
+        ], 200);       
     }
 
 //_____________________ User Search
     function search(UserSearchRequest $req){
-        if(!$req->user_search){
+        if (!$req->user_search) {
             return response([
-                'status'=>'Error',
-                'message'=>"not found"
-            ],400);
+                'status' => 'Error',
+                'message' => "not found"
+            ], 400);
         }
-        $user_search=str_replace(" ",'%',$req->user_search);
-        $search=userRepository::search($user_search);
+        $user_search = str_replace(" ", '%', $req->user_search);
+        $search = userRepository::search($user_search);
         return response([
-            'status'=>'success',
-            'search'=>$search
-        ],200);
+            'status' => 'success',
+            'search' => $search
+        ], 200);
     }
 
 //_____________________ Unauthenticated
-    function Unauthenticated(){
-        return response(['message'=>'please Login first'],401);
+    public function Unauthenticated()
+    {
+        return response([
+            'message'=>'please Login first'
+        ], 401);
     }
 
 
