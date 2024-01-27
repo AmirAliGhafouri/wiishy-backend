68cf461916cdc203af3bbd7a57eaac2801576a02
amirali <amiralighafouri80@gmail.com>
2024-01-27 08:29:47 +0330

    optimize likeRepository

--- wiishy/app/Repositories/likeRepository.php
+++ wiishy/app/Repositories/likeRepository.php
@@ -7,41 +7,59 @@ use Illuminate\Support\Facades\DB;
 
 class likeRepository
 {
-    static function check($gift_id , $user_id){
-        $like=giftlike::where(['gift_id'=>$gift_id , 'user_id'=>$user_id])->first();
-        if($like)
+    public static function check($gift_id , $user_id)
+    {
+        $like = giftlike::where([
+            'gift_id' => $gift_id,
+            'user_id' => $user_id
+        ])->first();
+
+        if ($like) {
             return true;
+        } 
         return false;
     }
 
-    static function like($gift_id , $user_id){
+    public static function like($gift_id, $user_id)
+    {
         giftlike::create([
-            'user_id'=>$user_id,
-            'gift_id'=>$gift_id
+            'user_id' => $user_id,
+            'gift_id' => $gift_id
         ]);
     }
 
-    static function dislike($gift_id , $user_id){
-        giftlike::where(['gift_id'=>$gift_id , 'user_id'=>$user_id])->delete();
+    public static function dislike($gift_id, $user_id)
+    {
+        giftlike::where([
+            'gift_id' => $gift_id,
+            'user_id' => $user_id
+        ])->delete();
     }
 
-    static function increase($gift_id){
+    public static function increase($gift_id)
+    {
         gift::where('id', $gift_id)->increment('gift_like');
     }
 
-    static function decrease($gift_id){
+    public static function decrease($gift_id)
+    {
         gift::where('id', $gift_id)->decrement('gift_like');
     }
 
-    static function count($gift_id){
-        return gift::where(['gift_id'=>$gift_id , 'gift_status'=>1])->first()->gift_like;
+    public static function count($gift_id)
+    {
+        return gift::where([
+            'gift_id' => $gift_id,
+            'gift_status' => 1
+        ])->first()->gift_like;
     }
 
-    static function list($gift_id){
+    public static function list($gift_id)
+    {
         return DB::table('giftlike')
-        ->join('users','giftlike.user_id','=','users.id')
-        ->where('giftlike.gift_id',$gift_id)
-        ->select('user_id','name','family','user_image_url','giftlike.created_at as like_date')
+        ->join('users', 'giftlike.user_id', '=', 'users.id')
+        ->where('giftlike.gift_id', $gift_id)
+        ->select('user_id', 'name', 'family', 'user_image_url', 'giftlike.created_at as like_date')
         ->get()->sortByDesc('like_date')->values();
     }
 }
